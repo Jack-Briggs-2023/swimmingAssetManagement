3276f290dec921f18be4b7fca1819aa8a8cb739a
senseiJack <jack.briggs@codeninjas.com>
2025-06-05 23:08:25 -0600

    start of creation

--- swimmingAssetManagement.py
+++ swimmingAssetManagement.py
@@ -1,2 +1,10 @@
-print("hello world")
-print("hello world")
+from tkinter import  *
+tkinterMainAsset = Tk()
+'''
+widgets are added here
+'''
+tkinterMainAsset.mainloop()
+
+label = Label(tkinterMainAsset, text='GeeksForGeeks.org!')
+label.pack()
+tkinterMainAsset.mainloop()
