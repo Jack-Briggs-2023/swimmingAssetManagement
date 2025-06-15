1fee85eb33520724432e60b82068e61203de5a71
senseiJack <jack.briggs@codeninjas.com>
2025-06-14 22:54:20 -0600

    added json files and general fixes

--- swimmingAssetManagement.py
+++ swimmingAssetManagement.py
@@ -1,3 +1,4 @@
+import math
 import tkinter as tk
 from tkinter import ttk
 import tkcalendar
@@ -5,12 +6,14 @@ from tkcalendar import DateEntry
 import tkinter.font as tkFont
 from datetime import date
 import time
+import json
 
 # Create main window
 tkinterMainAssetRoot = tk.Tk()
 tkinterMainAsseFrame = tk.Frame(tkinterMainAssetRoot)
 tkinterMainAsseFrame.pack(expand=True)
-tkinterMainAssetRoot.title("Swim Help")
+tkinterMainAssetRoot.title("Swim Help - Asset Management")
+tkinterMainAssetRoot.geometry("800x600")  # Set a default size
 
 windowSizeX = tkinterMainAssetRoot.winfo_width()
 windowSizeY = tkinterMainAssetRoot.winfo_height()
@@ -27,15 +30,17 @@ lifesavingSocietyLevels = {
     "Preschool": ["Preschool 1", "Preschool 2", "Preschool 3", "Preschool 4", "Preschool 5"],
     "Swimmer": ["Swimmer 1", "Swimmer 2", "Swimmer 3", "Swimmer 4", "Swimmer 5", "Swimmer 6"],
     "Swim Patrol": ["Rookie Patrol", "Ranger Patrol", "Star Patrol"],
-    "Bronze": ["Bronze Star", "Bronze Medallion", "Bronze Cross"]
+    "Bronze Medals": ["Bronze Star", "Bronze Medallion", "Bronze Cross"]
 }
 lifesavingSocietyLevelTitle = "Lifesaving Society Swim Program Level"
 
-class CreateNewLessonPlanWindow:
+
+
+class CreateNewClassWindow:
     def __init__(self, master, frame):
         self.master = master
         self.frame = frame
-        self.master.title("Create New Lesson Plan")
+        self.master.title("Create A New Class")
         self.master.attributes('-fullscreen', True)
         self.placeholderWidgetList = []
         self.placeholderList = []
@@ -45,17 +50,20 @@ class CreateNewLessonPlanWindow:
 
         self.master.focus_set()
 
-        label = tk.Label(self.frame, text="Create New Lesson Plan", font=("Roboto", 24))
+
+        # Create a label for the window
+        label = tk.Label(self.frame, text="Create A New Class", font=("Roboto", 24))
         label.pack(pady=defaultWidgeToWidgettPadding)
-        self.lessonNameEntry = tk.Entry(self.frame, font=("Roboto", 16), fg='grey')
-        self.lessonNameEntry.pack(pady=defaultLabelToWidgetPadding)
-        self.lessonNamePlaceholderText  = "Enter Lesson Name (for display only): "
-        self.lessonNameEntry.insert(0, self.lessonNamePlaceholderText )
+        self.classNameEntry = tk.Entry(self.frame, font=("Roboto", 16), fg='grey')
+        # self.classNameEntry.pack(fill='x', padx=10)
+        self.classNameEntry.pack(fill='x', padx=10, pady=defaultLabelToWidgetPadding)
+        self.classNamePlaceholderText  = "Enter Class Name (for display only) "
+        self.classNameEntry.insert(0, self.classNamePlaceholderText )
         # Bindings for placeholder text to clear and restore
-        self.lessonNameEntry.bind("<FocusIn>", self._clear_placeholder)
-        self.lessonNameEntry.bind("<FocusOut>", self._restore_placeholder)
-        self.placeholderWidgetList.append(self.lessonNameEntry)
-        self.placeholderList.append(self.lessonNamePlaceholderText)
+        self.classNameEntry.bind("<FocusIn>", self._clear_placeholder)
+        self.classNameEntry.bind("<FocusOut>", self._restore_placeholder)
+        self.placeholderWidgetList.append(self.classNameEntry)
+        self.placeholderList.append(self.classNamePlaceholderText)
 
         # Start date entry
         self.dateLabel = tk.Label(self.frame, text="Select Start Date:", font=("Roboto", 16))
@@ -73,6 +81,7 @@ class CreateNewLessonPlanWindow:
         # Bindings for placeholder text to clear and restore
         self.amountOfSetsEntry.bind("<FocusIn>", self._clear_placeholder)
         self.amountOfSetsEntry.bind("<FocusOut>", self._restore_placeholder)
+        # self.amountOfSetsEntry.bind("<KeyRelease>", self.check_if_number(self.amountOfSetsEntry)) # lambda e: 
         self.placeholderWidgetList.append(self.amountOfSetsEntry)
         self.placeholderList.append(self.amountOfSetsPlaceholderText)
 
@@ -84,39 +93,64 @@ class CreateNewLessonPlanWindow:
         self.levelGroupComboBox.bind("<<ComboboxSelected>>", self.on_combobox_selected)
         self.levelGroupComboBox.pack(pady=defaultWidgeToWidgettPadding)
 
+        # # Buttons for saving and exiting
+        # self.saveAndExitButton = tk.Button(self.frame, text="Save And Exit Class Plan", font=("Roboto", 16, "bold"), command=self.save_and_exit_class)
+        # self.saveAndExitButton.pack(side="left",pady=defaultWidgeToWidgettPadding)
+        # self.saveButton = tk.Button(self.frame, text="Save Class Plan", font=("Roboto", 16, "bold"), command=self.save_class)
+        # self.saveButton.pack(side="right",pady=defaultWidgeToWidgettPadding, padx=15)
+        # Create a new frame to hold both buttons and error message
+        self.buttonFrame = tk.Frame(self.frame)
+        self.buttonFrame.pack(pady=defaultWidgeToWidgettPadding)
 
-        self.saveAndExitButton = tk.Button(self.frame, text="Save And Exit Lesson Plan", font=("Roboto", 16, "bold"), command=self.save_and_exit_lesson_plan)
-        self.saveAndExitButton.pack(side="left",pady=defaultWidgeToWidgettPadding)
-        self.saveButton = tk.Button(self.frame, text="Save Lesson Plan", font=("Roboto", 16, "bold"), command=self.save_lesson_plan)
-        self.saveButton.pack(side="right",pady=defaultWidgeToWidgettPadding, padx=15)
+        # Add Save and Save+Exit buttons to this sub-frame
+        self.saveAndExitButton = tk.Button(self.buttonFrame, text="Save And Exit Class Plan", font=("Roboto", 16, "bold"), command=self.save_and_exit_class)
+        self.saveAndExitButton.pack(side="left", padx=10)
 
+        self.saveButton = tk.Button(self.buttonFrame, text="Save Class Plan", font=("Roboto", 16, "bold"), command=self.save_class)
+        self.saveButton.pack(side="right", padx=10)
+        
+        # Create a label for error messages
         self.errorLabel = None
         self.subLevelDropDown = None
 
-    def save_lesson_plan(self):
-        lessonName = self.lessonNameEntry.get()
-        startDate = self.startDateEntry.get()
-        amountOfSets = self.amountOfSetsEntry.get()
-        selectedLevel = self.selectedSubLevel.get()
 
-        if (lessonName.strip() == "" or lessonName == self.lessonNamePlaceholderText) or startDate.strip() == "" or amountOfSets.strip() == "" or selectedLevel == lifesavingSocietyLevelTitle:
-            self.display_error("Lesson name is required")
-            print("Please enter a valid lesson name")
+    def save_class(self):
+        try:
+            className = self.classNameEntry.get()
+            startDate = self.startDateEntry.get()
+            amountOfSets = self.amountOfSetsEntry.get()
+            selectedLevel = self.selectedSubLevel.get()
+        except AttributeError:
+            self.display_error("Please fill in all fields")
+            print("Please fill in all fields")
             return
-        print(f"Saved: {lessonName}, {startDate}, {amountOfSets}, {selectedLevel}")
+        saveData = {className, startDate, amountOfSets, selectedLevel}
+        for data in saveData:
+            if data.strip() == "" or data == self.classNamePlaceholderText or data == self.amountOfSetsPlaceholderText:
+                self.display_error("Please fill in all fields")
+                print("Please fill in all fields")
+                return
+        # if (className.strip() == "" or className == self.classNamePlaceholderText) or startDate.strip() == "" or amountOfSets.strip() == "" or selectedLevel == lifesavingSocietyLevelTitle:
+        #     self.display_error("Lesson name is required")
+        #     print("Please enter a valid lesson name")
+        #     return
+        # import os
+        #     if not os.path.exists("lesson_plans.json"):
+        #         with open("lesson_plans.json", "w") as file:
+        #             file.write("[]\n")
+        jsonExportData = {
+            "ClassName": className,
+            "startDate": startDate,
+            "amountOfSets": amountOfSets,
+            "selectedLevel": selectedLevel
+        }
+        with open("jsonClassData.json", "w") as file:
+            file.write(json.dumps(jsonExportData) + "\n")
+            # json.dumps(jsonExportData)
+        print(f"Saved: {className}, {startDate}, {amountOfSets}, {selectedLevel}")
 
-    def save_and_exit_lesson_plan(self):
-        self.save_lesson_plan()
-        # lessonNameEntry = self.lessonNameEntry.get()
-        # startDate = self.startDateEntry.get()
-        # amountOfSets = self.amountOfSetsEntry.get()
-        # print(f"Saved: {lessonNameEntry}, {startDate}, {amountOfSets}")
-        # self.lessonNameEntry.delete(0, "end")
-        # self.lessonNameEntry.insert(0, self.lessonNamePlaceholderText)
-        # self.lessonNameEntry.config(fg="grey")
-        # self.startDateEntry.set_date(date.today())
-        # self.amountOfSetsEntry.delete(0, "end")
-        # self.selectedGroupLevel.set('')
+    def save_and_exit_class(self):
+        self.save_class()
         reset_to_main_menu()
         self.master.focus_set()
 
@@ -131,10 +165,10 @@ class CreateNewLessonPlanWindow:
                     placeholderWidget.config(fg="black")
                     break
             count += 1
-        
-        # if self.lessonNameEntry.get() == self.lessonNamePlaceholderText:
-        #     self.lessonNameEntry.delete(0, "end")
-        #     self.lessonNameEntry.config(fg="grey")
+        # self.check_if_number(event)
+        # if self.classNameEntry.get() == self.classNamePlaceholderText:
+        #     self.classNameEntry.delete(0, "end")
+        #     self.classNameEntry.config(fg="grey")
 
     def _restore_placeholder(self, event):
         widget = event.widget
@@ -170,15 +204,63 @@ class CreateNewLessonPlanWindow:
             self.errorLabel.destroy()
             self.errorLabel = None
 
+    # def clear_all_fields(self):
+    #     self.classNameEntry.delete(0, "end")
+    #     self.classNameEntry.insert(0, self.classNamePlaceholderText)
+    #     self.classNameEntry.config(fg="grey")
+    #     self.startDateEntry.set_date(date.today())
+    #     self.amountOfSetsEntry.delete(0, "end")
+    #     self.amountOfSetsEntry.insert(0, self.amountOfSetsPlaceholderText)
+    #     self.amountOfSetsEntry.config(fg="grey")
+    #     self.selectedGroupLevel.set(lifesavingSocietyLevelTitle)
+    #     if hasattr(self, 'sublevelGroupComboBox'):
+    #         self.sublevelGroupComboBox.destroy()
+    def check_if_number(self, widget):
+        num = widget.get()
+        try:
+            int(num)
+        except ValueError:
+            widget.delete(0, "end")
+
+
+
+class ViewOldClassWindow:
+    def __init__(self, master, frame):
+        self.master = master
+        self.frame = frame
+        self.master.title("View Old Class")
+        self.master.attributes('-fullscreen', True)
+        self.placeholderWidgetList = []
+        self.placeholderList = []
+
+        for widget in self.frame.winfo_children():
+            widget.destroy()
+
+        label = tk.Label(self.frame, text="View Old Class", font=("Roboto", 24))
+        label.pack(pady=defaultWidgeToWidgettPadding)
+
+        # Placeholder for old class data
+        self.oldClassDataLabel = tk.Label(self.frame, text="Old Class Data Will Be Displayed Here", font=("Roboto", 16), fg='grey')
+        self.oldClassDataLabel.pack(pady=defaultWidgeToWidgettPadding)
+        with open("jsonClassData.json", "r") as file:
+            data = json.load(file)
+        if isinstance(data, list):
+            for classData in data:
+                print(classData)
+        elif isinstance(data, dict):
+            print(data)
+        else:
+            print("Unexpected JSON format")
 # Fullscreen
 tkinterMainAssetRoot.attributes('-fullscreen', True)
 
-def newPlanButton_action():
-    print("newPlanButton pressed!")
-    newPlan = CreateNewLessonPlanWindow(tkinterMainAssetRoot, tkinterMainAsseFrame)
+def newClassButton_action():
+    print("newClassButton pressed!")
+    newPlan = CreateNewClassWindow(tkinterMainAssetRoot, tkinterMainAsseFrame)
 
-def oldPlanButton_action():
+def oldClassButton_action():
     print("oldPlanButton pressed!")
+    viewOldPlan = ViewOldClassWindow(tkinterMainAssetRoot, tkinterMainAsseFrame)
 
 def exitButton_action():
     tkinterMainAssetRoot.destroy()
@@ -187,24 +269,24 @@ def exitButton_action():
 def reset_to_main_menu():
     for widget in tkinterMainAsseFrame.winfo_children():
         widget.destroy()
-    newPlanButton = tk.Button(tkinterMainAsseFrame, text="Create New Lesson Plan", font=("Roboto", 16), command=newPlanButton_action)
-    oldPlanButton = tk.Button(tkinterMainAsseFrame, text="View Old Lesson Plan", font=("Roboto", 16), command=oldPlanButton_action)
+    newClassButton = tk.Button(tkinterMainAsseFrame, text="Create A New Class", font=("Roboto", 16), command=newClassButton_action)
+    oldClassButton = tk.Button(tkinterMainAsseFrame, text="View Old Class", font=("Roboto", 16), command=oldClassButton_action)
     exitButton = tk.Button(tkinterMainAsseFrame, text="Exit", font=("Roboto", 16, "bold"), command=exitButton_action)
-    
-    newPlanButton.pack(pady=defaultWidgeToWidgettPadding)
-    oldPlanButton.pack(pady=defaultWidgeToWidgettPadding)
-    exitButton.pack(pady=defaultWidgeToWidgettPadding)
+    newClassButton.pack(pady=defaultWidgeToWidgettPadding)
+    oldClassButton.pack(pady=defaultWidgeToWidgettPadding)
+    exitButton.pack(pady=defaultWidgeToWidgettPadding)  
+    # newPlanButton.pack(pady=defaultWidgeToWidgettPadding)
+    # oldPlanButton.pack(pady=defaultWidgeToWidgettPadding)
+    # exitButton.pack(pady=defaultWidgeToWidgettPadding)
 
 
 
 tkinterMainAssetRoot.bind("<Escape>", lambda e: tkinterMainAssetRoot.destroy())
+reset_to_main_menu()
 
-newPlanButton = tk.Button(tkinterMainAsseFrame, text="Create New Lesson Plan", font=("Roboto", 16), command=newPlanButton_action)
-oldPlanButton = tk.Button(tkinterMainAsseFrame, text="View Old Lesson Plan", font=("Roboto", 16), command=oldPlanButton_action)
-exitButton = tk.Button(tkinterMainAsseFrame, text="Exit", font=("Roboto", 16, "bold"), command=exitButton_action)
 
-newPlanButton.pack(pady=defaultWidgeToWidgettPadding)
-oldPlanButton.pack(pady=defaultWidgeToWidgettPadding)
-exitButton.pack(pady=defaultWidgeToWidgettPadding)
+# newPlanButton.pack(pady=defaultWidgeToWidgettPadding)
+# oldPlanButton.pack(pady=defaultWidgeToWidgettPadding)
+# exitButton.pack(pady=defaultWidgeToWidgettPadding)
 
 tkinterMainAssetRoot.mainloop()
